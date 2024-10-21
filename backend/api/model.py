1c50cbaf9f3dca011efda32d2cd6dadca76200b9
nge9nike <maderb190007@sr.htlweiz.at>
2024-10-21 15:24:26 +0200

    Added documentation

--- backend/api/model.py
+++ backend/api/model.py
@@ -5,7 +5,9 @@ from pydantic import BaseModel
 class Issue(BaseModel):
     issue_type = str
     title = str
+    # task = report_time = null
     report_time = datetime.date
+    # bug = task_point = null
     task_point = int
 
 
